dbb9d705df978402317af72a4e2c97d41011daeb
cschleif <cschleif@gmail.com>
2016-07-12 13:29:00 -0700

    added wiki ajax with .don function

--- js/script.js
+++ js/script.js
@@ -45,21 +45,25 @@ function loadData() {
     //Wikipedia AJAX request
     var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
 
+    var wikiRequestTimeout = setTimeout(function() {
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
     $.ajax({
         url: wikiURL,
         dataType: "jsonp",
         // jsonp: "callback",
-        success: function(response) {
+    }).done(function(response) {
             var articleList = response[1];
 
             for (var i = 0; i < articleList.length; i++) {
                 articleStr = articleList[i];
                 var url = 'http://en.wikipedia.org/wiki/' + articleStr;
                 $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-
             };
-        }
-    })
+
+            clearTimeout(wikiRequestTimeout);
+        });
 
     return false;
 };
