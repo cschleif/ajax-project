ac1eb04e0fc85aa2cf7d4b89a2b1f7abbc6c6b99
cschleif <cschleif@gmail.com>
2016-07-12 11:56:26 -0700

    added google street view & NY Times API

--- js/script.js
+++ js/script.js
@@ -12,8 +12,33 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var streetStr = $("#street").val();
+    var cityStr = $("#city").val();
+    var address = streetStr + ", " + cityStr;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text("So, you want to live at " + address + "?");
+
+    var streetviewURL = "https://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + address + "";
+
+    $body.append('<img class="bgimg" src="' + streetviewURL + '">');
+
+    //NY Times Ajax request
+    var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=8eb3c7b101344c1e9c576b93c9fa3ca6'
+
+    $.getJSON(nytimesURL, function(data) {
+
+        $nytHeaderElem.text('New York Times Article About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' +
+                '<a href="'+ article.web_url +'">'+article.headline.main+
+                '</a>' +
+                '<p>' + article.snippet + '</p>'+
+                '</li>');
+        };
+    })
 
     return false;
 };
